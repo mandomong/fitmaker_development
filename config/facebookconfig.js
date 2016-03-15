6fb98f1d6222fd95be56931947e4387c798ecdc6
HYEMIN <hyeminkim@korea.ac.kr>
2016-03-15 13:00:50 +0900

    modify users.js and passportconfig.js for local log in and log out

--- config/facebookconfig.js
+++ config/facebookconfig.js
@@ -1,7 +1,9 @@
 module.exports = {
   "facebook" : {
-    "appId" : "1258606637486291",
-    "appSecret" : "6ede3fcd8525bda067d6bf93e0467c9c",
+    //"appId" : "1258606637486291",
+    //"appSecret" : "6ede3fcd8525bda067d6bf93e0467c9c",
+    "appID" : "457551064449409",
+    "appSecret" : "49193f5dc9eb89ef2cba8fd692370992",
     "callbackURL" : "http://localhost/auth/facebook/callback"
   }
 };
